4a79d52c03904cc487eee5ac74a2a63cea918dbe
Bart Dirkson <bhp_dirkson@hhotmail.com>
2020-11-20 14:36:20 +0100

    code1

--- TTD.js
+++ TTD.js
@@ -2,5 +2,13 @@
 // test test test
 // Gebruik git add -A - Gebruik git commit -m "boodschapje" - Voeg de remote toe aan je git repository - Gebruik git push origin master
 
+const getYieldForPlant = () => {
+    
+}
 
  
+module.exports = {
+    getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield
+};
