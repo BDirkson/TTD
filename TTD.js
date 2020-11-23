eaf02cce68db7ebe28188bf3d8d8bae994ce2e16
Bart Dirkson <bhp_dirkson@hhotmail.com>
2020-11-23 11:49:48 +0100

    test 2.3

--- TTD.js
+++ TTD.js
@@ -10,7 +10,7 @@ const getYieldForPlant = (plant) => {
 // functie 2 for test 2 - parameter is input. Function getYieldForPlant must return the yield of the corn * num. of crops.
 
 const getYieldForCrop = (input) => {
-    return getYieldForPlant (plant.yield) * input.numCrops;
+    return getYieldForPlant (input.crop) * input.numCrops;
 }
 
 module.exports = {
